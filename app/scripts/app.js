5bfa8db413c50d3ca8253f4e9aa9b9366a6395a9
pbellon <bellon.pierre@gmail.com>
2014-09-27 13:20:48 +0200

    Removed compiled javascript from repo

--- app/scripts/app.js
+++
@@ -1,7 +0,0 @@
-var neuPortfolio;
-
-angular.module('pbellonPortfolio.config', ['ui.router']);
-
-angular.module('pbellonPortfolio.controller', []);
-
-neuPortfolio = angular.module('pbellonPortfolio', ['pbellonPortfolio.config', 'pbellonPortfolio.controller', 'ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router']);
--- app/scripts/config/rootScope.js
+++
@@ -1,1 +0,0 @@
-angular.module('pbellonPortfolio').run(['$rootScope', function($rootScope) {}]);
