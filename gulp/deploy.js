4fd1606235db7b97594553f070a12e1c57f66c6a
pbellon <bellon.pierre@gmail.com>
2014-10-15 21:06:05 +0200

    Set deploy task simpler

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -3,9 +3,5 @@ var gulp = require('gulp');
 
 gulp.task('deploy', function () {
     return gulp.src('./dist/**/*')
-        .pipe(deploy({
-            origin: 'pbellon-gh',
-            branch: 'master',
-            remoteUrl: 'git@github.com:pbellon/pbellon.github.io.git'
-        }));
+        .pipe(deploy());
 });
